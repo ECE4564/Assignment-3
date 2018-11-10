c9e3f95067f031fe96062d9eebfdd63a38ff25ef
mpocta30 <40032123+mpocta30@users.noreply.github.com>
2018-11-10 13:19:16 -0500

    Update led.py

--- led.py
+++ led.py
@@ -4,17 +4,22 @@ import LED_PWM
 
 app = Flask(__name__)
 
+
 @app.route('/LED/on', methods=['POST'])
 def turnOn():
     led.turnOn()
+
     # Return message and code
     return "LED on"
+
 
 @app.route('/LED/off', methods=['POST'])
 def turnOff():
     led.turnOff()
+
     # Return message and code
     return "LED off"
+
 
 @app.route('/LED', methods=['POST'])
 def intensity():
@@ -31,11 +36,18 @@ def intensity():
     # Return message and code
     return "Successfully set " + color + "'s intensity to " + str(intensity)
 
-@app.route('/LED/off', methods=['GET'])
-def turnOff():
-    led.turnOff()
-    # Return message and code
-    return "LED off"
+
+@app.route('/LED/info', methods=['GET'])
+def info():
+    led_vals = {}
+
+    # Get all of the relevant information about LED
+    led_vals["red"] = led.red_int
+    led_vals["green"] = led.green_int
+    led_vals["blue"] = led.blue_int
+    led_vals["status"] = led.status
+
+    return jsonify({'Content': led_vals})
 
 
 if __name__ == '__main__':
