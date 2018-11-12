f171476dd87266275c77f3e35b75e125db91dda2
mpocta30 <40032123+mpocta30@users.noreply.github.com>
2018-11-11 21:54:53 -0500

    Update service.py

--- service.py
+++ service.py
@@ -3,6 +3,26 @@ from flask import Flask, request, jsonify, json
 import subprocess
 import os
 #import AuthDB
+from zeroconf import ServiceBrowser, ServiceStateChange, Zeroconf
+from time import sleep
+import logging
+import socket
+from typing import cast
+
+LED_IP = ""
+
+class MyListener (object):
+    def remove_service(self, zeroconf, type, name):
+        print("Service%sremoved" % (name,))
+    def add_service(self, zeroconf, type, name):
+        info = zeroconf.get_service_info(type, name)
+        try:
+            Name = info.name
+        except:
+            pass
+        if(Name == "LED._http._tcp.local."):
+            LED_IP = socket.inet_ntoa(cast(bytes, info.address))
+            zeroconf.close()
 
 app = Flask(__name__)
 
@@ -30,10 +50,10 @@ def upload_LED():
 
     print("Running " + filename + "...")
     # Need to change IP to zeroconf obtained IP
-    subprocess.Popen(["bash", os.path.join(app.config['UPLOAD_FOLDER'], filename), "192.168.1.36"])
+    subprocess.Popen(["bash", os.path.join(app.config['UPLOAD_FOLDER'], filename), LED_IP])
 
     # Return message and code
-    return "LED file successfully uploaded."
+    return "File successfully uploaded."
 
 
 @app.route('/upload/storage', methods=['POST'])
@@ -52,9 +72,21 @@ def upload_STORE():
     subprocess.Popen(["bash", os.path.join(app.config['UPLOAD_FOLDER'], filename), "192.168.1.36"])
 
     # Return message and code
-    return "Storage file successfully uploaded."
+    return "File successfully uploaded."
 
 
 if __name__ == '__main__':
     #auth = AuthDB()
+    zeroconf = Zeroconf()
+    listener = MyListener()
+    browser = ServiceBrowser(zeroconf, "_http._tcp.local.", listener)
+
     app.run(host= '0.0.0.0', port=5002)
+
+    # try:
+    #     while True:
+    #         sleep(0.1)
+    # except KeyboardInterrupt:
+    #     pass
+    # finally:
+    #     zeroconf.close()
