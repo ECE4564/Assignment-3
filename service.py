56a855ea6acf278e7e98ce0cddad0adc9ce7391c
mpocta30 <40032123+mpocta30@users.noreply.github.com>
2018-11-11 19:42:13 -0500

    Update service.py

--- service.py
+++ service.py
@@ -29,10 +29,31 @@ def upload_LED():
     bashfile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
     print("Running " + filename + "...")
+    # Need to change IP to zeroconf obtained IP
     subprocess.Popen(["bash", os.path.join(app.config['UPLOAD_FOLDER'], filename), "192.168.1.36"])
 
     # Return message and code
     return "File successfully uploaded."
+
+
+@app.route('/upload/storage', methods=['POST'])
+def upload_STORE():
+    try:
+        bashfile = request.files['file']
+        filename = bashfile.filename
+    except:
+        return 'Invalid request, no file included.'
+
+    # Saving file
+    bashfile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    print("Running " + filename + "...")
+    # Need to change IP to zeroconf obtained IP
+    subprocess.Popen(["bash", os.path.join(app.config['UPLOAD_FOLDER'], filename), "192.168.1.36"])
+
+    # Return message and code
+    return "File successfully uploaded."
+
 
 if __name__ == '__main__':
     #auth = AuthDB()
